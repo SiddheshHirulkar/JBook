e0978e799ceb373a449f0f78ffe3562ad979bdae
SiddheshHirulkar <siddhu3096@gmail.com>
2022-09-23 21:23:56 +0530

    Added page for sign in form.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,18 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faLaptopCode, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faLaptopCode, faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import { useHistory } from 'react-router-dom';
 
 import './navbar.css';
 import Sidebar from '../sidebar/sidebar';
 import { useTypedSelector } from '../../hooks/use-typed-selector';
+import { useActions } from '../../hooks/use-actions';
 
 const Navbar: React.FC = () => {
   const [sidebar, setSidebar] = useState(false);
   const [location, setLocation] = useState('/');
   const history = useHistory();
+  const { storePageLocation } = useActions();
 
   const pageLocation: any = useTypedSelector((state) => {
     return state.pageLocation;
@@ -20,13 +22,27 @@ const Navbar: React.FC = () => {
     setLocation(pageLocation);
   }, [pageLocation]);
 
+  useEffect(() => {
+    storePageLocation(history.location.pathname);
+  }, []);
+
   const showSidebar = () => setSidebar(!sidebar);
+  
+  const handleLogIn = () => {
+    history.push('/signIn');
+    storePageLocation(history.location.pathname);
+  };
+
+  const handleSignUp = () => {
+    history.push('/signUp');
+    storePageLocation(history.location.pathname);
+  };
 
   return (
     <Fragment>
-      <div className={`navbar ${location === '/' && 'navbar-fixed'}`}>
+      <div className={`navbar ${location !== '/cellList' ? 'navbar-fixed' : ''}`}>
         <span className="full-width">
-          {location === '/' ?
+          {location !== '/cellList' ?
             <div className="navbar-items-wrapper">
               <div>
                 <h3 onClick={() => history.push('/')} className="brand-title margin-left-30">
@@ -34,9 +50,15 @@ const Navbar: React.FC = () => {
                   <FontAwesomeIcon className="footer-title-icon" icon={faLaptopCode} />
                 </h3>
               </div>
-              <div className="sign-in-button margin-right-30">
-                Sign In
-                <FontAwesomeIcon className="sign-in-button-icon" icon={faUserCircle} />
+              <div className="navbar-buttons">
+                <div onClick={handleSignUp} className="navbar-btn sign-up-button margin-right-30">
+                  Sign Up
+                  <FontAwesomeIcon className="sign-in-button-icon" icon={faUserPlus} />
+                </div>
+                {location !== '/signIn' && (<div onClick={handleLogIn} className="navbar-btn sign-in-button margin-right-30">
+                  Log In
+                  <FontAwesomeIcon className="sign-in-button-icon" icon={faSignInAlt} />
+                </div>)}
               </div> 
             </div> :
             <div className="navbar-items-wrapper">
@@ -47,9 +69,15 @@ const Navbar: React.FC = () => {
                   <FontAwesomeIcon className="footer-title-icon" icon={faLaptopCode} />
                 </h3>
               </div>
-              <div className="sign-in-button margin-right-30">
-                Sign In
-                <FontAwesomeIcon className="sign-in-button-icon" icon={faUserCircle} />
+              <div className="navbar-buttons">
+                <div onClick={handleSignUp} className="navbar-btn sign-up-button margin-right-30">
+                  Sign Up
+                  <FontAwesomeIcon className="sign-in-button-icon" icon={faUserPlus} />
+                </div>
+                <div onClick={handleLogIn} className="navbar-btn sign-in-button margin-right-30">
+                  Log In
+                  <FontAwesomeIcon className="sign-in-button-icon" icon={faSignInAlt} />
+                </div>
               </div>
             </div>}
         </span>
---
+++ src/components/signIn/login-form.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import './login-form.css';
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  
+  const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) :void => {
+    e.preventDefault();
+
+    // set email and password back to default
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmitForm}>
+      <div className="form-field">
+        <label className="field-label" htmlFor="emailField">Email</label>
+        <input 
+          className="field-input"
+          id="emailField"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@email.com"
+        />
+      </div>
+      <div className="form-field">
+        <label className="field-label" htmlFor="passwordField">Password</label>
+        <input
+          className="field-input"
+          id="passwordField"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Use strong password"
+        />
+      </div>
+      <div className="form-button">
+        <button
+          type="submit"
+          className="form-button-submit"
+        >
+          Log In
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
